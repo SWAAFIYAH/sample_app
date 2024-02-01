a940d126fe208f3cef877d20b405bdbc25d1f3b9
SWAAFIYAH <swaafiyah9@gmail.com>
2024-02-01 12:24:08 +0300

    updated|

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class _HomeState extends State<Home> {
             Text('Coding level',
               style: TextStyle(
                 color: Colors.red,
-                letterSpacing: 2.0,
+                letterSpacing: 1.0,
               ),
             ),
             Text('$codingLevel',
